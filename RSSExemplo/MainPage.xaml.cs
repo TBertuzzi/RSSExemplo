c461aa11c817195b7ad8c97a40186c1250f5833e
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-10-11 11:39:22 -0300

    :sparkles: Subindo exemplo

---
+++ RSSExemplo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSSExemplo.Models;
+using RSSExemplo.ViewModels;
+using Xamarin.Forms;
+
+namespace RSSExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+
+        void ListView_OnItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            var selectedItem = (RssData)e.Item;
+            Navigation.PushAsync(new RSSWebViewPage(selectedItem.Link));
+        }
+    }
+}
